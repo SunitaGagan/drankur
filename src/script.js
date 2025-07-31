86b8f6419574b283908987fd94a7e1f2a5291665
yadavsunita <sunita.sun15april@gmail.com>
2025-07-31 12:57:57 +0530

    update

--- src/script.js
+++ src/script.js
@@ -1,27 +1,11 @@
- // Load header
- fetch('header.html')
-     .then(res => res.text())
-     .then(data => {
-         document.getElementById('header').innerHTML = data;
-     });
+$('.hero-slider').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
 
- // Load footer
- fetch('footer.html')
-     .then(res => res.text())
-     .then(data => {
-         document.getElementById('footer').innerHTML = data;
-     });
- $('.hero-slider').owlCarousel({
-     loop: true,
-     margin: 0,
-     nav: true,
-     dots: true,
-     items: 1,
-     autoplay: true,
-     autoplayTimeout: 4000,
-     autoplayHoverPause: true,
-     navText: [
-         '<span class="arrow-left">‹</span>',
-         '<span class="arrow-right">›</span>'
-     ]
- });
+});
