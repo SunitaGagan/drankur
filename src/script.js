d67c6a7e2d4192b025431a57dda7d7a58ab4efd7
yadavsunita <sunita.sun15april@gmail.com>
2025-09-25 15:16:22 +0530

    update

--- src/script.js
+++ src/script.js
@@ -141,3 +141,18 @@ $(document).ready(function() {
         adjustSidebar();
     });
 })(jQuery);
+
+async function loadHTML(id, file) {
+    try {
+        let res = await fetch(file);
+        let text = await res.text();
+        document.getElementById(id).innerHTML = text;
+    } catch (err) {
+        console.error("Error loading HTML:", err);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    loadHTML("header", "header.html");
+    loadHTML("footer", "footer.html");
+});
